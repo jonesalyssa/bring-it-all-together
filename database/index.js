602f2c919f2fa3671177d7e839fcd918e6a797c4
D-Money Tucker <d-moneytucker@D-Moneys-MacBook-Pro.local>
2025-02-04 20:21:16 -0600

    fixed url

--- database/index.js
+++ database/index.js
@@ -7,13 +7,3 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-const db = new Pool({
-  connectionString:
-    process.env.DATABASE_URL || "postgres://localhost:3000/bringitalltogether",
-});
-
-async function query(sql, params, callback) {
-  return db.query(sql, params, callback);
-}
-
-module.exports = { query };
